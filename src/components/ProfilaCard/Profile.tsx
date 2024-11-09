f5f5686bb397814e6c0ebbb0b1a1ff8959e6310b
k0nAtbl4 <gsbeshsvzb620@gmail.com>
2024-11-09 21:47:15 +0300

    change avatar

--- src/components/ProfilaCard/Profile.tsx
+++ src/components/ProfilaCard/Profile.tsx
@@ -2,10 +2,10 @@ import "./Profile.css"
 import React, { useState, useEffect } from "react";
 
 function Profile() {
-    const NotFoundAvatar = "https://avatars.githubusercontent.com/u/276340?v=4?s=400";
+    // const NotFoundAvatar = "https://avatars.githubusercontent.com/u/276340?v=4?s=400";
 
 
-    const [user, setUser] = useState("https://avatars.githubusercontent.com/u/276340?v=4?s=400");
+    const [user, setUser] = useState("https://avatars.cloudflare.steamstatic.com/c966426a3f4bed2801d175aff9cf49fcf1267076_full.jpg");
 
     useEffect(() => {
         fetch("https://avatars.fastly.steamstatic.com/c966426a3f4bed2801d175aff9cf49fcf1267076_full.jpg", {
